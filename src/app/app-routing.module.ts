5a8ef0ff5330a7bfc6273bd9ea2b06b904de8b1b
Vladyslav Charuk <vlad.charu@gmail.com>
2021-02-18 21:23:00 +0200

    added another part of project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthPageComponent } from './components/auth-page/auth-page.component';
+import { CoursesPageComponent } from './components/courses-page/courses-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'login', component: AuthPageComponent },
+  { path: 'courses', component: CoursesPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
